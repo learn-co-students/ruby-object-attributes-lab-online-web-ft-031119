8b757826df95974c5f841763c0ea4dfc20179329
Eric Storey <estorey@gmail.com>
2019-03-19 17:57:52 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name=(dog_name)
+    @name=dog_name
+  end
+  
+  def name
+    @name
+  end
+  
+  
+  def job=(pers_job)
+    @job=pers_job
+  end
+  
+  def job
+    @job
+  end
+end
